39b7fb945afaee516da0cd9f36d3e25da5886a3d
Jordan Whitten <jordanwa1947@gmail.com>
2020-01-28 10:33:21 -0700

    Adds createCards method and start method

--- src/Game.js
+++ src/Game.js
@@ -2,8 +2,14 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
+const Round = require('../src/Round');
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound;
+  }
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -12,6 +18,23 @@ class Game {
 
   printQuestion(round) {
     util.main(round);
+  }
+
+  createCards() {
+    return prototypeQuestions.map(card => {
+      let id, question, answers, correctAnswer;
+      ({ id, question, answers, correctAnswer } = card)
+      return new Card(id, question, answers, correctAnswer);
+    });
+  }
+
+  start() {
+    const cards = this.createCards();
+    const deck = new Deck(cards);
+    const round = new Round(deck);
+    this.currentRound = round;
+    this.printMessage(deck, round);
+    this.printQuestion(round);
   }
 }
 
